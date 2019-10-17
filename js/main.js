1b8d67b9acf79334312b41f856046f4f6721148b
DKelly <kelly13493@gmail.com>
2019-10-17 01:15:17 -0400

    Adding mergesort (including permutation and split/merge function)

--- js/main.js
+++ js/main.js
@@ -237,6 +237,90 @@ var sorting = (function() {
         quicksort(aa, pivot_type, pivot + 1, right);
       }
       
+      function check_perm(perm) {        
+        var n = perm.length;
+        var used = {};
+        for (var i = 0; i < n; i++) {
+          if (used[perm[i]]) return false;
+          used[perm[i]] = true;
+        }
+        for (var i = 0; i < n; i++) if (!used[i]) return false;
+        return true;
+      }
+    
+    
+      function perm_to_swaps(perm) {
+        
+        if (!check_perm(perm)) {
+          console.log(perm);
+          throw "Invalid permutation";
+        }
+        var n = perm.length;
+        var used = [];
+        for (var i = 0; i < n; i++) used.push(false);
+        var transpositions = [];
+    
+        for (var i = 0; i < n; i++) {
+          if (used[i]) continue;
+          var cur = i;
+          if (perm[i] == i) used[i] = true;
+          while (!used[perm[cur]]) {
+            transpositions.push([cur, perm[cur]]);
+            used[cur] = true;
+            cur = perm[cur];
+          }
+        }
+    
+        return transpositions;
+      }
+    
+    
+      function mergesort(aa, left, right) {
+        if (typeof(left) === 'undefined') left = 0;
+        if (typeof(right) === 'undefined') right = aa.length() - 1;
+    
+        if (left >= right) return;
+        
+        var mid = Math.floor((left + right) / 2);
+    
+        if (right - left > 1) {
+          mergesort(aa, left, mid);
+          mergesort(aa, mid + 1, right);
+        }    
+        
+        var next_left = left;
+        var next_right = mid + 1;
+        var perm = [];
+        for (var i = left; i <= right; i++) {
+          var choice = null;
+          if (next_left <= mid && next_right <= right) {
+            if (aa.lessThan(next_left, next_right)) {
+              choice = 'L';
+            } else {
+              choice = 'R';
+            }
+          } else if (next_left > mid) {
+            choice = 'R';
+          } else if (next_right > right) {
+            choice = 'L';
+          }
+          if (choice === 'L') {
+            perm.push(next_left - left);
+            next_left++;
+          } else if (choice === 'R') {
+            perm.push(next_right - left);
+            next_right++;
+          } else {
+            throw 'Should not get here'
+          }
+        }
+    
+        var swaps = perm_to_swaps(perm);
+        for (var i = 0; i < swaps.length; i++) {
+          aa.swap(swaps[i][0] + left, swaps[i][1] + left);
+        }
+      }
+    
       var algorithms = {
         'bubblesort': bubblesort,
         'selectionsort': selectionsort,       
